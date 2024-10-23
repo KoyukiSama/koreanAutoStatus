a5ea6b17798846a9c5027615dc8cdeb7c43ab87e
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-10-24 00:43:39 +0200

    add struct KoreanWordList, Word also add serialize fprintf

--- korean.c
+++ korean.c
@@ -1,7 +1,7 @@
 #include <stdio.h>;
 
 typedef char* String; // create String data type
-
+const char* STRUCT_WORD_FORMAT = "(%i, %s, %c)\n";
 
 // SETTINGS -------------------------------------------------------------------!
 // in quotes, put path-to-file of words
@@ -10,15 +10,26 @@ const String FILE_PATH = "/home/katarina/repos/KimchiReader-Auto-Status/words.da
 
 // SETTINGS -------------------------------------------------------------------!
 
-
+struct KoreanWordList {
+    int WordCount;
+    struct Word* WordList;
+};
 struct Word {
     int count;
     String word;
-    String status;
+    char status; // U, S, K respectively unknown, seen, known, these are to indicate if the person has the following words on seen or on known
 };
 
-void serialize(FILE* file, String FILE_PATH) {
+void serialize(FILE* file, String FILE_PATH, struct KoreanWordList KoreanWordList) {
     file = fopen(FILE_PATH, "w+");
+
+    int WordCount = KoreanWordList.WordCount; // set count
+    struct Word* WordList = KoreanWordList.WordList;
+
+    fprintf(file, "[%i]", WordCount); // print wordCount to file
+    for(int i; i<WordCount; i++) { // print struct Word to file
+        fprintf(file, STRUCT_WORD_FORMAT, WordList[i].count, WordList[i].word, WordList[i].status);
+    }
 }
 
 struct Word* deserialise() {
