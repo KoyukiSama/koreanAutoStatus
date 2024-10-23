4f9bc378ed6e0ec9e21e53ccbbd0a832ee937c50
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-10-24 01:15:15 +0200

    add deserialise

--- korean.c
+++ korean.c
@@ -1,14 +1,18 @@
 #include <stdio.h>;
 
 typedef char* String; // create String data type
-const char* STRUCT_WORD_FORMAT = "(%i, %s, %c)\n";
+const char* STRUCT_WORDCOUNT_FORMAT = "%i";
+const char* STRUCT_WORD_FORMAT_IN = "(%i, %c, %50[^,])";
+const char* STRUCT_WORD_FORMAT_OUT = "(%i, %c, %s)\n";
+
 
 // SETTINGS -------------------------------------------------------------------!
 // in quotes, put path-to-file of words
 // "/home/(name)/KimchiReader-Auto-Status/words.dat"
-const String FILE_PATH = "/home/katarina/repos/KimchiReader-Auto-Status/words.dat";
-
+const String FILE_PATH_WORDS = "/home/katarina/repos/KimchiReader-Auto-Status/words.dat";
+const String FILE_PATH_WORDCOUNT = "/home/katarina/repos/KimchiReader-Auto-Status/wordcount.dat";
 // SETTINGS -------------------------------------------------------------------!
+
 
 struct KoreanWordList {
     int WordCount;
@@ -16,24 +20,45 @@ struct KoreanWordList {
 };
 struct Word {
     int count;
-    String word;
     char status; // U, S, K respectively unknown, seen, known, these are to indicate if the person has the following words on seen or on known
+    String word;
 };
 
-void serialize(FILE* file, String FILE_PATH, struct KoreanWordList KoreanWordList) {
-    file = fopen(FILE_PATH, "w+");
+void getWordCount;
 
+void serialize(String FILE_PATH_WORDS, struct KoreanWordList KoreanWordList) { // save to file
+    // open file with error handling
+    FILE* file = fopen(FILE_PATH_WORDS, "w+");
+    if (file == NULL) { perror("Error opening file"); return; }
+
+    // set structs for easier access
     int WordCount = KoreanWordList.WordCount; // set count
     struct Word* WordList = KoreanWordList.WordList;
 
+    // print structs to file
     fprintf(file, "[%i]", WordCount); // print wordCount to file
-    for(int i; i<WordCount; i++) { // print struct Word to file
-        fprintf(file, STRUCT_WORD_FORMAT, WordList[i].count, WordList[i].word, WordList[i].status);
+    for(int i = 0; i<WordCount; i++) { // print struct Word to file
+        fprintf(file, STRUCT_WORD_FORMAT_OUT, WordList[i].count, WordList[i].status, WordList[i].word);
     }
+
+    fclose(file);
 }
 
-struct Word* deserialise() {
+struct KoreanWordList* deserialise(String FILE_PATH, struct KoreanWordList KoreanWordList) { // load objects from file
+    // open file with error handling
+    FILE* file = fopen(FILE_PATH, "w+");
+    if (file == NULL) { perror("Error opening file"); return; }
 
+    // set structs for easier access
+    int WordCount = KoreanWordList.WordCount; // set count
+    struct Word* WordList = KoreanWordList.WordList;
+
+    // scan file
+    for (int i = 0; i<WordCount; i++) {
+        fscanf(file, STRUCT_WORD_FORMAT_IN, WordList[i].count, WordList[i].status, WordList[i].word);
+    }
+
+    fclose(file);
 }
 
 int main(int argc, char *argv[]) {
